c837d5f1f01f4881eeb9b711b611bd777e450578
Hérlon Manollo C Guedes <manollo.guedes11@gmail.com>
2020-01-14 23:41:17 -0500

    feat(api): api creation

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json({devs});
+  },
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+  
+    let dev = await Dev.findOne({github_username});
+
+    if(!dev) {
+
+      const response = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+      const { name = login, avatar_url, bio } = response.data;
+    
+      const techsArray = parseStringAsArray(techs);
+    
+      const location = {
+        type: 'Point', 
+        coordinates: [longitude, latitude],
+      }
+    
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      });
+    }
+
+    return res.json({
+      dev
+    });
+  },
+
+  async update(req, res) {
+    try {
+      const { github_username } = req.params;
+      const devChanges = req.body;
+
+      const dev = await Dev.findOneAndUpdate(
+        { github_username },
+        devChanges,
+        {
+          new: true
+        }
+      );
+
+      return res.json({ dev });
+    } catch(error) {
+      console.error(error);
+      res.json({error: 'error'});
+    }
+  },
+
+  async destroy(req, res) {
+    const { github_username } = req.params;
+    
+    let dev = await Dev.findOneAndDelete(github_username);
+
+    res.json({ dev });
+  },
+};
