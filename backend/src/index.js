aecde2f958099a61f108df3fad7dd1beae40890a
Hérlon Manollo C Guedes <manollo.guedes11@gmail.com>
2020-01-17 16:47:10 -0500

    feat(websocket): using socket.io to provide a real time updating

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 require('dotenv/config');
 
 const app = express();
+const server = http.Server(app); //isolating the http protocol to use socket and http requests
+
+setupWebSocket(server);
 
 mongoose.connect(process.env.DB_STR, {
   useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
